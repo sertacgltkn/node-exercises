c4a4b4b19cdf9bcfeda9563b9e51b9a422cd3ebf
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-17 13:58:37 +0300

    ejs print

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const app = express()
 
 app.set("view engine","ejs")
 
+const products = [
+  {id: 1, name: "iphone 14", price: 30000},
+  {id: 2, name: "iphone 15", price: 40000},
+  {id: 3, name: "iphone 16", price: 50000}
+]
+
 app.use("/products/:id", function(req, res) {
   res.render("product-details")
 })
 
 app.use("/products", function(req, res) {
-  res.render("Products")
+  res.render("Products", {
+    products: products
+  })
 })
 
 app.use("/", function(req, res) {
